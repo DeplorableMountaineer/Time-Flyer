8f3c9eb112babb6445d8c3f6f068265f12d34881
tdvance <deplorable.mountaineer@gmail.com>
2020-05-01 15:09:26 -0400

    Added player sprite and background that follows the camera's position (but not rotation) to simulate parallax

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+    [SerializeField] private Camera camera = null;
+
+    private void Reset() {
+        if(camera == null) camera = Camera.main;
+    }
+
+    private void Update() {
+        Vector2 pos = camera.transform.position;
+        transform.position = pos;
+    }
+}
