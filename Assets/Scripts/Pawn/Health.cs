7e17cb641fe8afe7c098f05a7f22ee9d63e76dcb
tdvance <deplorable.mountaineer@gmail.com>
2020-05-05 11:27:07 -0400

    Added flame effect to visualize health of ships

--- Assets/Scripts/Pawn/Health.cs
+++ Assets/Scripts/Pawn/Health.cs
@@ -6,11 +6,13 @@ namespace Pawn {
         [SerializeField] private float startingHealth = 100;
         [SerializeField] private float maxHealth = 100;
         [SerializeField] private float healingRate = 10;
+        [SerializeField] private ParticleSystem flames = null;
 
         public void TakeDamage(float amount) {
             _health -= amount;
             if(_health < 0) Die();
             enabled = _health < maxHealth;
+            SetFlameProperties();
         }
 
         public void Die() {
@@ -23,17 +25,36 @@ namespace Pawn {
         private void Start() {
             _health = startingHealth;
             enabled = _health < maxHealth;
+            SetFlameProperties();
         }
 
         private void Update() {
             float newHealth = _health + healingRate*Time.smoothDeltaTime;
             if(newHealth >= maxHealth) {
-                enabled = false;
+                if(enabled) {
+                    SetFlameProperties();
+                    enabled = false;
+                }
+
                 _health = maxHealth;
                 return;
             }
 
+            if(Mathf.Abs(_health - newHealth) < .0001f) return;
+            SetFlameProperties();
             _health = newHealth;
+        }
+
+        private void SetFlameProperties() {
+            if(!flames) return;
+            if(!enabled) {
+                if(flames.isPlaying) flames.Stop();
+                return;
+            }
+
+            if(!flames.isPlaying) flames.Play();
+            ParticleSystem.MainModule main = flames.main;
+            main.maxParticles = Mathf.FloorToInt(Mathf.Pow(1000, 1 - _health/maxHealth));
         }
     }
 }
