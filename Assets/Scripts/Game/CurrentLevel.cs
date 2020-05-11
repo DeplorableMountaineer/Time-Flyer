d48a36dca711c1a0e6e3e6299dbf516ebda2ad86
tdvance <deplorable.mountaineer@gmail.com>
2020-05-11 10:57:52 -0400

    Added more levels and time scaling

---
+++ Assets/Scripts/Game/CurrentLevel.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Game {
+    public class CurrentLevel : MonoBehaviour {
+        [SerializeField] private string timeZome = "3000";
+        [SerializeField] private float timeScale = 1;
+
+        private void Start() {
+            GameObject go = GameObject.FindGameObjectWithTag("Time Zone Text");
+            if(go) {
+                TextMeshProUGUI text = go.GetComponent<TextMeshProUGUI>();
+                if(text) text.text = timeZome;
+            }
+
+            Time.timeScale = timeScale;
+        }
+    }
+}
--- Assets/Scripts/Game/Levels.cs
+++ Assets/Scripts/Game/Levels.cs
@@ -12,7 +12,6 @@ namespace Game {
 
         public void LoadStartScene() {
             SceneManager.LoadScene("Title");
-
         }
     }
 }
