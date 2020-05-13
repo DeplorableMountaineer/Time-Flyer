6c3d85b14a7b4f669350092f85c3f45257cb6ff7
tdvance <deplorable.mountaineer@gmail.com>
2020-05-13 18:29:55 -0400

    Minor bug fixes

--- Assets/Scripts/Game/CurrentLevel.cs
+++ Assets/Scripts/Game/CurrentLevel.cs
@@ -1,27 +1,32 @@
 ﻿using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Game {
     /**
      * Data for the current level
      */
     public class CurrentLevel : MonoBehaviour {
-        [FormerlySerializedAs("TimeZone")] [FormerlySerializedAs("timeZome")] [SerializeField]
-        private string timeZone = "3000";
+        [SerializeField] private string timeZone = "3000";
         [SerializeField] private float timeScale = 1;
 
         private void Start() {
             GameObject go = GameObject.FindGameObjectWithTag("Time Zone Text");
             if(go) {
                 TextMeshProUGUI text = go.GetComponent<TextMeshProUGUI>();
+                if(Game.Instance.CurrentLevel >= 10) {
+                    int time = Mathf.RoundToInt(Random.Range(-1000, 1000))*100;
+                    if(time == 0) timeZone = "1 AD";
+                    else if(time > 0 && time < 1001) timeZone = time.ToString() + " AD";
+                    else if(time > 0) timeZone = time.ToString();
+                    else timeZone = (-time).ToString() + " BC";
+                }
+
                 if(text) text.text = timeZone;
             }
 
             //Speed things up on later levels
-            if(Game.Instance.CurrentLevel < 11) Time.timeScale = timeScale;
-            else Time.timeScale = timeScale*Mathf.Ceil(Game.Instance.CurrentLevel/10f);
-            Debug.Log("Time Scale: " + Time.timeScale);
+            if(Game.Instance.CurrentLevel < 10) Time.timeScale = timeScale;
+            else Time.timeScale = timeScale + Mathf.Ceil(Game.Instance.CurrentLevel/10f);
         }
     }
 }
